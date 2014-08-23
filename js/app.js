2392fe419f0be24950a068bd5b3d69b1b4ed8f5c
Hannah Nordgren <hey@hereshannahs.info>
2014-08-23 17:36:07 -0400

    broke something

--- js/app.js
+++ js/app.js
@@ -9,13 +9,23 @@ App.Router.map(function() {
 
 App.PostsRoute = Ember.Route.extend({
   model: function() {
-    return posts;
+    return $.getJSON('http://tomdale.net/api/get_recent_posts/?callback=?').then(function(data)
+      return data.posts.map(function(post) {
+        post.body = post.content;
+        return post;
+      });
+    });
   }
 });
 
 App.PostRoute = Ember.Route.extend({
   model: function(params) {
-    return posts.findBy('id', params.post_id);
+    return $.getJSON('http://tomdale.net/api/get_recent_post/?id=').then(function(data) {
+      return data.posts.map(function(post) {
+        data.post.body = data.post.content;
+        return post;
+      });
+    });
   }
 });
 
@@ -32,30 +42,15 @@ App.PostController = Ember.ObjectController.extend({
       this.set("isEditing", false);
     }
   }
-})
+});
 
-Ember.Handlebars.helper('format-date', function(date){
+Ember.Handlebars.helper('format-date', function(date) {
   return moment(date).fromNow();
 });
 
 var showdown = new Showdown.converter();
 
-Ember.Handlebars.helper('format-markdown', function(input){
+Ember.Handlebars.helper('format-markdown', function(input) {
   return new Handlebars.SafeString(showdown.makeHtml(input));
 });
 
-var posts = [{
-  id: '1',
-  title: "Hannah is awesome",
-  author: { name: "Brenda" },
-  date: new Date('12-27-2012'),
-  excerpt: "Candy canes marshmallow soufflé applicake sweet marzipan bear claw. Cookie sweet roll jelly jelly-o icing soufflé caramels bonbon caramels.",
-  body: "Candy canes marshmallow soufflé applicake sweet marzipan bear claw. Cookie sweet roll jelly jelly-o icing soufflé caramels bonbon caramels. Pie cheesecake carrot cake toffee tart toffee ice cream bear claw tootsie roll. Soufflé pastry soufflé jelly beans chocolate cake dragée. Macaroon icing carrot cake applicake croissant soufflé chocolate wafer. Powder candy canes chocolate wafer. Toffee dessert muffin. Cookie jelly-o donut chocolate cake bear claw chocolate pastry chocolate cake chocolate bar. Sweet danish cotton candy sweet topping. Candy canes bear claw chocolate cake icing tart powder bear claw oat cake. Oat cake chocolate bear claw lemon drops ice cream ice cream. Muffin toffee macaroon cookie danish muffin croissant cookie jujubes. Sesame snaps powder lemon drops caramels wafer marzipan. Powder chocolate topping danish tart."
-}, {
-  id: '2',
-  title: "Brenda is awesome",
-  author: { name: "Hannah" },
-  date: new Date('12-30-2012'),
-  excerpt: "Donut pie gummies donut apple pie candy canes. Apple pie ice cream caramels candy pie jujubes chocolate bonbon tootsie roll. Pie bonbon jelly beans wafer cheesecake chocolate cake biscuit soufflé.",
-  body: "Donut pie gummies donut apple pie candy canes. Apple pie ice cream caramels candy pie jujubes chocolate bonbon tootsie roll. Pie bonbon jelly beans wafer cheesecake chocolate cake biscuit soufflé. Oat cake jelly beans unerdwear.com muffin candy canes. Powder toffee gummi bears muffin cupcake marshmallow lollipop sesame snaps applicake. Unerdwear.com applicake halvah chocolate bar brownie. Cupcake biscuit icing croissant candy lollipop. Pie cheesecake gingerbread. Toffee danish cheesecake bear claw macaroon biscuit cotton candy sugar plum. Liquorice liquorice carrot cake sweet caramels oat cake liquorice chocolate bar candy. Donut tart tart. Tootsie roll caramels pie sesame snaps chocolate chocolate chupa chups. Tiramisu cookie caramels gingerbread."
-}];
